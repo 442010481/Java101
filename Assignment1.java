0a6064f2b7fb78a1cfecae40914f32bdc715f57c
Mohammed9921 <91115081+Mohammed9921@users.noreply.github.com>
2021-09-26 06:08:20 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//Your Name mohammed ahmed alhartoumi
+ 
+//Your ID 442010481
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+  
+  
+   float N1 = 4.5f;
+   float N2 = 1.5f;
+   
+   System.out.println(N1+N2);
+   System.out.println(N1-N2);
+     System.out.println(N1*N2);
+    System.out.println(N1/ N2);
+   System.out.println(N1%N2); 
+   System.out.println(N1>N2);
+     System.out.println(N1>=N2);
+    System.out.println(N1<N2); 
+     System.out.println(N1<=N2);
+     System.out.println(N1!=N2);
+     System.out.println(N1==N2);
+  }
+}
