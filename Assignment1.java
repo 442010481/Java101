3ca52e29456613701bfd431c10044e34e0794651
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-30 00:24:18 +0300

    fix the 5 issues

--- Assignment1.java
+++ Assignment1.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
 //Your Name mohammed ahmed alhartoumi
- 
+
 //Your ID 442010481
+// your grade is 2.5/5
+//@ change the class name Main to match the file name Assignment1 (golden rule)
 class Main {
-  public static void main(String[] args) {
-      //Start your code 
-  
-  
-   float N1 = 4.5f;
-   float N2 = 1.5f;
-   
-   System.out.println(N1+N2);
-   System.out.println(N1-N2);
-     System.out.println(N1*N2);
-    System.out.println(N1/ N2);
-   System.out.println(N1%N2); 
-   System.out.println(N1>N2);
-     System.out.println(N1>=N2);
-    System.out.println(N1<N2); 
-     System.out.println(N1<=N2);
-     System.out.println(N1!=N2);
-     System.out.println(N1==N2);
-  }
+    public static void main(String[] args) {
+        //Start your code
+
+        //@ let the user enter the float number from the keyboard
+        //@ ask the user to enter a number
+        float N1 = 4.5f;
+        //@ let the user enter the float number from the keyboard
+        //@ ask the user to enter a number
+        float N2 = 1.5f;
+
+        System.out.println(N1 + N2);
+        System.out.println(N1 - N2);
+        System.out.println(N1 * N2);
+        System.out.println(N1 / N2);
+        System.out.println(N1 % N2);
+        System.out.println(N1 > N2);
+        System.out.println(N1 >= N2);
+        System.out.println(N1 < N2);
+        System.out.println(N1 <= N2);
+        System.out.println(N1 != N2);
+        System.out.println(N1 == N2);
+    }
 }
