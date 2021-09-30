48204652ea4088de05d8153f33097ce8a1a2995f
442010481 <91115081+442010481@users.noreply.github.com>
2021-09-30 14:11:29 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 
+
 //Your Name mohammed ahmed alhartoumi
 
 //Your ID 442010481
 // your grade is 2.5/5
 //@ change the class name Main to match the file name Assignment1 (golden rule)
-class Main {
+          class Main {
     public static void main(String[] args) {
-        //Start your code
-
-        //@ let the user enter the float number from the keyboard
-        //@ ask the user to enter a number
-        float N1 = 4.5f;
-        //@ let the user enter the float number from the keyboard
-        //@ ask the user to enter a number
-        float N2 = 1.5f;
-
-        System.out.println(N1 + N2);
-        System.out.println(N1 - N2);
+          double N1 , N2 ;
+           Scanner myscan = new Scanner(System.in);
+           
+      N1  = myscan.nextDouble();
+       N2 = myscan.nextDouble();
+       System.out.println(N1+N2);
+       System.out.println(N1-N2);
         System.out.println(N1 * N2);
         System.out.println(N1 / N2);
         System.out.println(N1 % N2);
@@ -27,5 +24,7 @@ class Main {
         System.out.println(N1 <= N2);
         System.out.println(N1 != N2);
         System.out.println(N1 == N2);
+    
+    
     }
 }
