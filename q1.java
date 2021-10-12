28d9abdad4ea75d9cb72aebccfb14d28019af0f9
442010481 <91115081+442010481@users.noreply.github.com>
2021-10-12 08:57:09 +0300

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class q1
+{
+	public static void main(String[] args) {
+			    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter your num");
+	    float num =scan.nextFloat();
+	    if( num %2==0)
+	    System.out.println("Odd");
+	    else 
+	    System.out.println("Evan");
+	}
+
+
+	}
