f95bdf8bbbe41f8dcbaad04eff67ce2cccab8331
442010481 <91115081+442010481@users.noreply.github.com>
2021-12-11 16:56:34 +0300

    Create Degree.java

---
+++ Degree.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+     float degree;
+      System.out.println("Enter your degree");
+      degree =scan.nextFloat();
+     if(degree<= 100 && degree>=90 ){
+     System.out.println(degree+": is A");}
+     else if ( degree<90 && degree >= 80){
+     System.out.println(degree+ ": is B");}
+     else if ( degree<80 && degree>=70){
+       System.out.println(degree+" is c");}
+       else if (degree<70 && degree>= 60){
+       System.out.println(degree+"is D");}
+       else if (degree<60){
+           System.out.println("sorry your fail");
+       }
+        
+           else{
+              System.out.println("Error"); 
+           }
+  }
+}
