8c7874489ebfc4843b2a07d97a59b9dba374eeba
442010481 <91115081+442010481@users.noreply.github.com>
2021-12-12 16:27:31 +0300

    Create powFunction.java

---
+++ powFunction.java
@@ -0,0 +1,14 @@
+public class powFunction {
+  static int pow(int x ) {
+    return x*x;
+  }
+  
+ 
+  
+  public static void main(String[] args) {
+    int myNum1 = pow(8);
+ 
+    System.out.println( myNum1);
+    
+  }
+}
