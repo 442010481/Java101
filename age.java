bac0bfb2f1ba88c0cb545c7b087db6fd3f6a0fde
442010481 <91115081+442010481@users.noreply.github.com>
2021-12-12 15:54:11 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter  your brith year ");
+    int brith; 
+    brith=scan.nextInt();
+     int age;
+     age = 2020 - brith;
+     System.out.println( age);}}
