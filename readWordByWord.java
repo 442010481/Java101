00432b11a7185c1b28a00029fb90773eaa7e6990
442010481 <91115081+442010481@users.noreply.github.com>
2021-12-15 01:34:16 +0300

    Create  readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,11 @@
+
+public class readWordByWord {
+        public static void main(String[] args) {
+            
+                String txt = "mohmmed\nahmed ";
+                System.out.println(txt);
+
+
+
+        }
+}
