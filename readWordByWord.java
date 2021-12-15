7b695fb6ff59b4ea7dafd52b79c328651abc448b
442010481 <91115081+442010481@users.noreply.github.com>
2021-12-15 14:34:54 +0300

    Update readWordByWord.java

--- readWordByWord.java
+++ readWordByWord.java
@@ -2,10 +2,16 @@
 public class readWordByWord {
         public static void main(String[] args) {
             
-                String txt = "mohmmed\nahmed ";
-                System.out.println(txt);
+  
+    String name = "mohmmaed ahmed";
+    for (int x = 0; x < 15; ++x)
+      {
+
+	System.out.println (name.charAt(x));
+      }
+
+}}
 
 
 
-        }
-}
+        
