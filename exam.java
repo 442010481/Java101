b444f49f1ab2e7113dce2c651676cdfcbbd6e5ef
442010481 <91115081+442010481@users.noreply.github.com>
2021-12-21 08:18:45 +0300

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+     for (int cunter = 1; cunter <= 100 ; ++cunter ){
+         if (cunter %2 != 0){
+         System.out.println(cunter+" %2 is odd " );}
+         else {
+             System.out.println(cunter+"  %2 is even");
+     }
+     
+	}
+}
+}
